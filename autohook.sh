d3ea210bd31ca829fb57c8c0c9a178899adda1ea
Nikolaos Kakouros <nkak@kth.se>
2019-10-23 19:02:08 +0200

    Formats with shfmt

--- autohook.sh
+++ autohook.sh
@@ -6,101 +6,87 @@
 # Version:  2.1.1
 # Website:  https://github.com/nkantar/Autohook
 
-
 echo() {
-    builtin echo "[Autohook] $@";
+  builtin echo "[Autohook] $@"
 }
-
 
 install() {
-    hook_types=(
-        "applypatch-msg"
-        "commit-msg"
-        "post-applypatch"
-        "post-checkout"
-        "post-commit"
-        "post-merge"
-        "post-receive"
-        "post-rewrite"
-        "post-update"
-        "pre-applypatch"
-        "pre-auto-gc"
-        "pre-commit"
-        "pre-push"
-        "pre-rebase"
-        "pre-receive"
-        "prepare-commit-msg"
-        "update"
-    )
+  hook_types=(
+    "applypatch-msg"
+    "commit-msg"
+    "post-applypatch"
+    "post-checkout"
+    "post-commit"
+    "post-merge"
+    "post-receive"
+    "post-rewrite"
+    "post-update"
+    "pre-applypatch"
+    "pre-auto-gc"
+    "pre-commit"
+    "pre-push"
+    "pre-rebase"
+    "pre-receive"
+    "prepare-commit-msg"
+    "update"
+  )
 
-    repo_root="$(git rev-parse --show-toplevel)"
-    hooks_dir="$repo_root/.git/hooks"
-    cd "${0%/*}"
-    autohook_linktarget="$(pwd)/autohook.sh"
-    echo "$autohook_linktarget"
+  repo_root="$(git rev-parse --show-toplevel)"
+  hooks_dir="$repo_root/.git/hooks"
+  cd "${0%/*}"
+  autohook_linktarget="$(pwd)/autohook.sh"
+  echo "$autohook_linktarget"
 
-    for hook_type in "${hook_types[@]}"
-    do
-        hook_symlink="$hooks_dir/$hook_type"
-        ln -sf "$autohook_linktarget" "$hook_symlink"
-    done
+  for hook_type in "${hook_types[@]}"; do
+    hook_symlink="$hooks_dir/$hook_type"
+    ln -sf "$autohook_linktarget" "$hook_symlink"
+  done
 }
-
 
 main() {
-    calling_file="$(basename "$0")"
+  calling_file="$(basename "$0")"
 
-    if [[ "$calling_file" == "autohook.sh" ]]
-    then
-        command="$1"
-        if [[ "$command" == "install" ]]
-        then
-            install
-        fi
-    else
-        repo_root="$(git rev-parse --show-toplevel)"
-        hook_type="$calling_file"
-
-        if [[ "${AUTOHOOK_HOOKS_DIR=''}" != '' ]]
-        then
-            symlinks_dir="$AUTOHOOK_HOOKS_DIR/$hook_type"
-        else
-            symlinks_dir="$repo_root/hooks/$hook_type"
-        fi
-
-        files=("$symlinks_dir"/*)
-        number_of_symlinks="${#files[@]}"
-        if [[ "$number_of_symlinks" == 1 ]]
-        then
-            if [[ "$(basename "${files[0]}")" == "*" ]]
-            then
-                number_of_symlinks=0
-            fi
-        fi
-        echo "Looking for $hook_type scripts to run...found $number_of_symlinks!"
-        if [[ "$number_of_symlinks" -gt 0 ]]
-        then
-            hook_exit_code=0
-            for file in "${files[@]}"
-            do
-                scriptname="$(basename "$file")"
-                echo "BEGIN $scriptname"
-                eval "$file" &> /dev/null
-                script_exit_code="$?"
-                if [[ "$script_exit_code" != 0 ]]
-                then
-                  hook_exit_code="$script_exit_code"
-                fi
-                echo "FINISH $scriptname"
-            done
-            if [[ "$hook_exit_code" != 0 ]]
-            then
-              echo "A $hook_type script yielded negative exit code $hook_exit_code"
-              exit "$hook_exit_code"
-            fi
-        fi
+  if [[ "$calling_file" == "autohook.sh" ]]; then
+    command="$1"
+    if [[ "$command" == "install" ]]; then
+      install
     fi
-}
+  else
+    repo_root="$(git rev-parse --show-toplevel)"
+    hook_type="$calling_file"
 
+    if [[ "${AUTOHOOK_HOOKS_DIR=''}" != '' ]]; then
+      symlinks_dir="$AUTOHOOK_HOOKS_DIR/$hook_type"
+    else
+      symlinks_dir="$repo_root/hooks/$hook_type"
+    fi
+
+    files=("$symlinks_dir"/*)
+    number_of_symlinks="${#files[@]}"
+    if [[ "$number_of_symlinks" == 1 ]]; then
+      if [[ "$(basename "${files[0]}")" == "*" ]]; then
+        number_of_symlinks=0
+      fi
+    fi
+    echo "Looking for $hook_type scripts to run...found $number_of_symlinks!"
+    if [[ "$number_of_symlinks" -gt 0 ]]; then
+      hook_exit_code=0
+      for file in "${files[@]}"; do
+        scriptname="$(basename "$file")"
+        echo "BEGIN $scriptname"
+        eval "$file" &>/dev/null
+        script_exit_code="$?"
+        if [[ "$script_exit_code" != 0 ]]; then
+          hook_exit_code="$script_exit_code"
+        fi
+        echo "FINISH $scriptname"
+      done
+      if [[ "$hook_exit_code" != 0 ]]; then
+        echo "A $hook_type script yielded negative exit code $hook_exit_code"
+        exit "$hook_exit_code"
+      fi
+    fi
+  fi
+}
 
 main "$@"
